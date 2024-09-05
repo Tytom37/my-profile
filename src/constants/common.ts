fab6b451e32d873fb0f03bf1309b36c418f8c127
Tytom37 <thitomangelo@gmail.com>
2024-09-06 07:47:00 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Thitom Angelo D. Buligan';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  "I'm a 4th-year BSIT student at Mater Dei, passionate about technology and coding. I enjoy tackling challenges in software development, exploring new tech trends, and working on projects that enhance my skills.";
+export const ABOUT_TEXT_2 =
+  "I’m learning how to build and maintain web applications from the frontend to the backend. I enjoy working with different teams to design, create, and launch solutions that help solve real-world problems and meet project goals.";
+export const ABOUT_TEXT_3 =
+  'An eager student exploring both frontend and backend development, working towards building complete web applications. Currently learning various programming languages, frameworks, and databases, and excited to keep growing my skills.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Passionate student learning to build websites and web applications using modern JavaScript tools and frameworks. Gaining knowledge of frontend and backend technologies, as well as database management, while working on personal and school projects.';
+export const RESUME_TEXT_2 =
+  'Student exploring website and web app development with modern JavaScript tools. Building knowledge in frontend, backend, and database management through projects and coursework.';
